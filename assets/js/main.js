208f2e9fd1e0dff868497daeb05f06b8cc0dc6d6
Nitin Singhal <nitinsinghal139@gmail.com>
2021-03-29 02:29:10 +0530

    Update xml http request response parsing to json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,18 @@
 function shortUrl(){
-	var response = getData();
-	var shortUrl = response.url;
-	var sample = "https://tinyurl123.herokuapp.com/" + shortUrl;
-	document.getElementById("demo").innerHTML = sample.link(sample);
-}
-
-function getData(){
-	var url = document.getElementById("shorturl").value;
+	var longurl = document.getElementById("shorturl").value;
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "https://tinyurl123.herokuapp.com/generate", false);
+	xhr.open("POST", "https://tinyurl123.herokuapp.com/generate", true);
 	xhr.setRequestHeader("Content-Type", "application/json");
-	//xhr.setRequestHeader("Access-Control-Allow-Origin" , "*");
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4 && xhr.status === 200) {
-			//console.log(xhr.responseText);
-			return xhr.responseText;
+			var resu = JSON.parse(this.response);
+			console.log(resu);
+			var tiny = resu.url;
+			console.log(tiny);
+			var sample = "https://tinyurl123.herokuapp.com/" + tiny;
+			document.getElementById("demo").innerHTML = sample.link(sample);
 		}
 	};
-	var data = JSON.stringify({"url": url});
-	xhr.send(null);
+	var data = JSON.stringify({"url": longurl});
+	xhr.send(data);
 }
